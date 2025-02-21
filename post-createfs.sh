cd16d27948999583999386b39c2f3fc456d71abd
Marc Lainez <marc.lainez@gmail.com>
2025-02-21 08:51:26 +0100

    wip

--- post-createfs.sh
+++ post-createfs.sh
@@ -2,12 +2,7 @@
 
 set -e
 
-mkdir -p $BINARIES_DIR/boot/extlinux
-cp $NERVES_DEFCONFIG_DIR/extlinux/extlinux.conf $BINARIES_DIR/boot/extlinux/
-cp $BINARIES_DIR/zImage $BINARIES_DIR/boot/
-cp $BINARIES_DIR/*.dtb $BINARIES_DIR/boot/
-
-genext2fs -B 1024 -b 51200 -d $BINARIES_DIR/boot/ $BINARIES_DIR/boot.img
+$NERVES_DEFCONFIG_DIR/scripts/create-boot-img.sh
 
 FWUP_CONFIG=$NERVES_DEFCONFIG_DIR/fwup.conf
 
